f4ea1b557215220f4ba4586ee2fb9092d4c482ba
Thomas Lyons <talyons22@gmail.com>
2020-01-29 18:42:58 -0800

    WIP

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,21 @@
+import Search from './Search.js';
+import VideoPlayer from './VideoPlayer.js';
+import VideoList from './VideoList.js';
+import exampleVideoData from '../data/exampleVideoData.js';
+
 var App = () => (
   <div>
     <nav className="navbar">
       <div className="col-md-6 offset-md-3">
-        <div><h5><em>search</em> view goes here</h5></div>
+        <div><h5><em><Search /></em> view goes here</h5></div>
       </div>
     </nav>
     <div className="row">
       <div className="col-md-7">
-        <div><h5><em>videoPlayer</em> view goes here</h5></div>
+        <div><h5><em><VideoPlayer /></em> view goes here</h5></div>
       </div>
       <div className="col-md-5">
-        <div><h5><em>videoList</em> view goes here</h5></div>
+        <div><h5><em><VideoList videos={exampleVideoData} /></em> view goes here</h5></div>
       </div>
     </div>
   </div>
@@ -21,3 +26,4 @@ var App = () => (
 export default App;
 
 
+// DOM
--- src/components/VideoList.jsx
+++ src/components/VideoList.jsx
@@ -1,10 +1,11 @@
-var VideoList = () => (
+
+
+var VideoList = (props) => (
+
   <div className="video-list">
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
+    {props.videos.map(video =>
+      <VideoListEntry video={video} />
+    )}
   </div>
 );
 
@@ -14,6 +15,9 @@ VideoList.propTypes = {
   videos: React.PropTypes.array.isRequired
 };
 
+
 // In the ES6 spec, files are "modules" and do not share a top-level scope.
 // `var` declarations will only exist globally where explicitly defined.
 export default VideoList;
+
+
