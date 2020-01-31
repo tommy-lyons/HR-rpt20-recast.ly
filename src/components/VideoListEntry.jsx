107b0e51a8d0c2278c1ea146514d6174cfea1ff4
Valeriia Ten <tenv279@gmail.com>
2020-01-30 23:53:56 -0500

    WIP

--- src/components/VideoListEntry.jsx
+++ src/components/VideoListEntry.jsx
@@ -1,6 +1,11 @@
 var VideoListEntry = (props) => (
 
-  <div className="video-list-entry media">
+  handleClick = () => {
+    // when we click on the div, we want to change the state of the VideoPlayer component so that it plays the
+    // video corresponding to
+  }
+
+  <div className="video-list-entry media" onClick={this.handleClick}>
     <div className="media-left media-middle">
       <img className="media-object" src={props.video.snippet.thumbnails.default.url} alt="" />
     </div>
@@ -20,3 +25,5 @@ VideoListEntry.propTypes = {
 // In the ES6 spec, files are "modules" and do not share a top-level scope
 // `var` declarations will only exist globally where explicitly defined
 export default VideoListEntry;
+
+
