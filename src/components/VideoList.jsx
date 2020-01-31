3e2c4adf7eabf9f0e14d66ed8c286d34026edbd0
Valeriia Ten <tenv279@gmail.com>
2020-01-30 22:46:40 -0500

    Complete creating a dynamic Video List component

--- src/components/VideoList.jsx
+++ src/components/VideoList.jsx
@@ -3,7 +3,7 @@ import VideoListEntry from './VideoListEntry.js';
 var VideoList = (props) => (
   <div className="video-list">
     {props.videos.map(video =>
-      <VideoListEntry video={video} />
+      <VideoListEntry key={video.id.videoId} video={video} />
     )}
   </div>
 );
@@ -19,4 +19,4 @@ VideoList.propTypes = {
 // `var` declarations will only exist globally where explicitly defined.
 export default VideoList;
 
-
+// Pass VideoListEntry tests
