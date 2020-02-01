93660af836470e824e28d786d7cb7f0b2af87f91
Thomas Lyons <talyons22@gmail.com>
2020-02-01 13:23:20 -0800

    Complete creating reusable API helper

--- src/lib/searchYouTube.js
+++ src/lib/searchYouTube.js
@@ -1,5 +1,25 @@
 var searchYouTube = (options, callback) => {
-  // TODO
+
+  // using code format from chatterbox ajax request
+  $.ajax({
+    url: 'https://www.googleapis.com/youtube/v3/search',
+    type: 'GET',
+    data: {
+      part: 'snippet',
+      q: options.query,
+      maxResults: options.max,
+      key: options.key,
+      type: 'video'
+    },
+    contentType: 'application/json',
+    success: function (data) {
+      // console.log(data);
+      callback(data.items);
+    },
+    error: function(error) {
+      console.error('', error);
+    }
+  });
 };
 
 export default searchYouTube;
