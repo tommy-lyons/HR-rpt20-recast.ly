7fe6eba00a8531798196a7a591826832ce67e06a
Thomas Lyons <talyons22@gmail.com>
2020-02-01 11:57:06 -0800

    Complete connecting app to the YouTube API

--- notes.js
+++ notes.js
@@ -1,2 +1,9 @@
-// "start":"babel . --out-dir compiled --presets=react --ignore=node_modules,compiled --source-maps inline && live-server"
+// this didn't work, not sure what was going on
+//  "start":"babel . --out-dir compiled --presets=react --ignore=node_modules,compiled --source-maps inline --watch && live-server"
+
+// this seemed to work, but might have created issues, so I seperated them into two commands and ran them in two seperate terminals
+// "start":"live-server & babel . --out-dir compiled --presets=react --ignore=node_modules,compiled --source-maps inline --watch"
+
+//
+//
 
